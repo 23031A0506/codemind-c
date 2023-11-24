7e4fc0eb4879c02a2544cca734acdc9b88fc2a0b
23031A0506 <147791690+23031A0506@users.noreply.github.com>
2023-11-24 10:45:43 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     while(n>2)         if(n%2==1 || n==2)     {         printf("Prime");         break;     }     else     {         printf("Not Prime");         break;     } }

---
+++ Prime.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    while(n>2)
+        if(n%2==1 || n==2)
+    {
+        printf("Prime");
+        break;
+    }
+    else
+    {
+        printf("Not Prime");
+        break;
+    }
+}
