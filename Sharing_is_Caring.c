169df0d9611ab80fe240c6962925b4b3d388f23f
23031A0506 <147791690+23031A0506@users.noreply.github.com>
2024-01-18 08:45:27 +0530

    #include<stdio.h> int main() {     int n,m;     scanf("%d%d",&n,&m);     if((m*(m+1)/2)<=n)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Sharing_is_Caring.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,m;
+    scanf("%d%d",&n,&m);
+    if((m*(m+1)/2)<=n)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
